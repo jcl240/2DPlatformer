52139ff595b5402156e918c0e14fc221c94749d1
Joseph Ledford <jcl240@georgetown.edu>
2016-09-20 12:05:46 +0200

    scripts etc.

---
+++ Assets/Scripts/hasCoin.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hasCoin : MonoBehaviour {
+	public bool occupied;
+	// Use this for initialization
+	void Awake () {
+		occupied = false;
+	}
+
+}

f2926b41e3f923b4b6861b3937e41bc72629e1cd
ryankenney42 <ryankenney42@hotmail.com>
2016-09-20 12:23:08 +0200

    Initialize

--- Assets/Scripts/Doorway1.cs
+++ Assets/Scripts/Doorway1.cs
@@ -5,29 +5,34 @@ using UnityEngine.SceneManagement;
 public class Doorway1 : MonoBehaviour {
 	
 	public GameObject player;
-	private float doorX = transform.position.x;
-	private float doorY = transform.position.y;
+	private float doorX;
+	private float doorY;
 
 	// Use this for initialization
 	void Start () {
+
+		doorX = gameObject.transform.position.x;
+		doorY = gameObject.transform.position.y;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		float moveVertical = Input.GetAxis ("Vertical");
 	
 	}
 
 
-	public void UseDoor() {
 
-		float moveVertical = Input.GetAxis ("Vertical");
+	public void UseDoor(float moveVertical) {
+
 		float playerX = player.transform.position.x;
 		float playerY = player.transform.position.y;
-		if(moveVertical == 1 && ((playerX > doorX - 1)||(playerX < doorX + 1)) &&
-			((playerY > doorY - 1)||(playerY < doorY + 1)){
-				SceneManager.LoadScene (1);
-			}
+		if (moveVertical == 1 && ((playerX > doorX - 1) || (playerX < doorX + 1)) &&
+		   ((playerY > doorY - 1) || (playerY < doorY + 1))) {
+		}
+				
+
 
 	}
 

310ce2d26ba082994d1696d6735f488fe3f7d607
ryankenney42 <ryankenney42@hotmail.com>
2016-09-20 16:37:50 +0200

    Initialize

--- Assets/Scripts/Doorway1.cs
+++ Assets/Scripts/Doorway1.cs
@@ -4,13 +4,16 @@ using UnityEngine.SceneManagement;
 
 public class Doorway1 : MonoBehaviour {
 	
-	public GameObject player;
+	//public GameObject player;
+	SimplePlatformController player;
+	public GameObject gameCamera;
 	private float doorX;
 	private float doorY;
+	float cameraRotation;
 
 	// Use this for initialization
 	void Start () {
-
+		player = GetComponent<SimplePlatformController> (); //get the player controller
 		doorX = gameObject.transform.position.x;
 		doorY = gameObject.transform.position.y;
 	
@@ -19,19 +22,22 @@ public class Doorway1 : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float moveVertical = Input.GetAxis ("Vertical");
-	
+		float playerX = player.transform.position.x;
+		float playerY = player.transform.position.y;
+		if (moveVertical == 1 && ((playerX > doorX - 1) || (playerX < doorX + 1)) &&
+			((playerY > doorY - 1) || (playerY < doorY + 1))) {
+		}
 	}
 
 
 
 	public void UseDoor(float moveVertical) {
 
-		float playerX = player.transform.position.x;
-		float playerY = player.transform.position.y;
-		if (moveVertical == 1 && ((playerX > doorX - 1) || (playerX < doorX + 1)) &&
-		   ((playerY > doorY - 1) || (playerY < doorY + 1))) {
-		}
-				
+		player.transform.position = -Vector3.forward;
+		gameCamera.transform.position = -Vector3.forward;
+		cameraRotation = gameCamera.transform.rotation.z;
+		gameCamera.transform.position.z = cameraRotation + 180;
+		//gameCamera.transform.rotation = Quaternion.AngleAxis (rotation, Vector3.forward);
 
 
 	}
